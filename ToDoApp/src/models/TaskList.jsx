43321bafdac153d9b6fa0837ad703cc85b490a78
pedroespinelg <pedroespi.09@gmail.com>
2024-09-30 22:00:41 +0200

    Testing a small modification, no effect on program

--- ToDoApp/src/models/TaskList.jsx
+++ ToDoApp/src/models/TaskList.jsx
@@ -5,12 +5,14 @@ export default function TaskList({ taskList, setTaskList }) {
   return (
     <ul className={styles.tasklist}>
       {taskList.map((item) => (
-        <TaskItem
-          key={item.name}
-          task={item}
-          taskList={taskList}
-          setTaskList={setTaskList}
-        />
+        <div>
+          <TaskItem
+            key={item.name}
+            task={item}
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+        </div>
       ))}
     </ul>
   );
